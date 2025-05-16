3a06d89b4f1dc598da6399f145bbd27747b7defd
Nusiba <nusibaalnabhani@gmail.com>
2025-05-16 17:48:59 +0400

    test

---
+++ shopping.py
@@ -0,0 +1,32 @@
+#shopping 
+
+food_list = []
+price_list = []
+total = 0
+
+while True:
+    food = input("Enter the food you buy (when you finish enter done): ")
+    if food.lower() == "done":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: OMR"))
+
+    food_list.append(food)
+    price_list.append(price)
+
+print("-----YOUR CART-----")
+for food in food_list:
+    print(food, end=" ")
+
+for price in price_list:
+    total += price
+
+print()
+print(f"Your total is:{total}OMR")
+
+#print(f"Your shopping list is: {food[0]}, {price[0]}")
+    
+    
+
+
+
